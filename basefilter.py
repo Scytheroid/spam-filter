c2d7c5e584b26623c4501928b9b4a56d1fe675b0
houskkam <kamila.hou@seznam.cz>
2020-12-08 22:27:35 +0100

    basefilter according to step 4

---
+++ basefilter.py
@@ -0,0 +1,12 @@
+﻿import os
+class BaseFilter:
+
+    def train(self, dir_path):
+        pass
+        #os.remove(dir_path + '/!truth.txt')
+    
+    def test(self, dir_path):
+        raise NotImplemented_Error("Každý filtr se trénuje jinak")
+
+if __name__ == "__main__":
+    pass
