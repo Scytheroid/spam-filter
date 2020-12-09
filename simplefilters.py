300fd56d2720d9be88c1bc2a8d09e7a2e13bc072
houskkam <kamila.hou@seznam.cz>
2020-12-09 10:55:05 +0100

    no raw string

--- simplefilters.py
+++ simplefilters.py
@@ -4,7 +4,7 @@ from basefilter import BaseFilter
 
 class NaiveFilter(BaseFilter):
     def test(self, dir_path):
-        with open(dir_path + r"/!prediction.txt","w+", encoding = "utf-8") as f:
+        with open(dir_path + "/!prediction.txt","w+", encoding = "utf-8") as f:
           for fname in os.listdir(dir_path):
               if not fname.startswith('!'):
                   f.write(fname + " OK\n")
