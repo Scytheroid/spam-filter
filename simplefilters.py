a7f02a8ddd18332e69dbce484b4c174b277b3194
houskkam <kamila.hou@seznam.cz>
2020-12-09 10:42:57 +0100

    changing open as to with open

--- simplefilters.py
+++ simplefilters.py
@@ -4,24 +4,22 @@ from basefilter import BaseFilter
 
 class NaiveFilter(BaseFilter):
     def test(self, dir_path):
-        f = open(dir_path + "/!prediction.txt","w+", encoding = "utf-8")
-        for fname in os.listdir(dir_path):
-            if not fname.startswith('!'):
-                f.write(fname + " OK\n")
-        f.close()
+        with open(dir_path + r"/!prediction.txt","w+", encoding = "utf-8") as f:
+          for fname in os.listdir(dir_path):
+              if not fname.startswith('!'):
+                  f.write(fname + " OK\n")
 
 class ParanoidFilter(BaseFilter):
     def test(self, dir_path):
-        f = open(dir_path + "/!prediction.txt","w+", encoding = "utf-8")
-        for fname in os.listdir(dir_path):
-            if not fname.startswith('!'):
-                f.write(fname + " SPAM\n")
-        f.close() 
+        with open(dir_path + "/!prediction.txt","w+", encoding = "utf-8") as f:
+            for fname in os.listdir(dir_path):
+                if not fname.startswith('!'):
+                    f.write(fname + " SPAM\n")
 
 class RandomFilter(BaseFilter):
     def test(self, dir_path):
-        f = open(dir_path + "/!prediction.txt","w+", encoding = "utf-8")
-        for fname in os.listdir(dir_path):
-            if not fname.startswith('!'):
-                f.write(fname + " " + random.choice(['HAM', 'SPAM']) + "\n")
-        f.close()
+        with open(dir_path + "/!prediction.txt","w+", encoding = "utf-8") as f:
+            for fname in os.listdir(dir_path):
+                if not fname.startswith('!'):
+                    f.write(fname + " " + random.choice(['HAM', 'SPAM']) + "\n")
+           
