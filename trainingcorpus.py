6f9aee0cec6ea7ee6f344307b1647e9d4183a2df
houskkam <kamila.hou@seznam.cz>
2020-12-10 12:12:33 +0100

    cleaning code

--- trainingcorpus.py
+++ trainingcorpus.py
@@ -1,3 +1,4 @@
+
 from utils import read_only, read_classification_from_file
 from constants import POSITIVE, NEGATIVE, TRUTHFILE, PREDFILE
 
@@ -5,34 +6,31 @@ class TrainingCorpus:
     def __init__(self, path_to_mails):
         self.path_to_mails = path_to_mails
         
-    def get_class(self, ename):
+    def is_tag(self, ename, whichtag):
         rel_path = self.path_to_mails + '/'
         is_ham = read_classification_from_file(rel_path + TRUTHFILE)
-        if (is_ham[ename] == NEGATIVE):
+        if (is_ham[ename] == whichtag):
+            return True
+        else:
+            return False        
+            
+    def get_class(self, ename):
+        if (self.is_tag(ename, NEGATIVE) == True):
             return NEGATIVE
         else:
             return POSITIVE 
         
     def is_ham(self, ename):
-        rel_path = self.path_to_mails + '/'
-        is_ham = read_classification_from_file(rel_path + TRUTHFILE)
-        if (is_ham[ename] == NEGATIVE):
-            return True
-        else:
-            return False     
+        return self.is_tag(ename, NEGATIVE)
                  
     def is_spam(self, ename):
-        rel_path = self.path_to_mails + '/'
-        is_ham = read_classification_from_file(rel_path + TRUTHFILE)
-        if (is_ham[ename] == POSITIVE):
-            return True
-        else:
-            return False 
+        return self.is_tag(ename, POSITIVE) 
     
     def spams(self):
         yield read_only(self.path_to_mails, POSITIVE)
         
     def hams(self):
         yield read_only(self.path_to_mails, NEGATIVE)
+           
                
     
