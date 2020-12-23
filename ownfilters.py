36f2ed88cb3a7c725cbacc8dafd9b850cbfc8fe0
houskkam <kamila.hou@seznam.cz>
2020-12-23 17:03:33 +0100

    hasWord update

--- ownfilters.py
+++ ownfilters.py
@@ -4,19 +4,15 @@ from collections import Counter
 
 class HasWordFilter(BaseFilter):
     def test(self, email, the_word):
-        emails = email.split()
-        occurance = Counter(emails)
-        print(occurance)
-        for word in email:
-            word.lower()
-            occurance[word] += 1
+        words = email.split()
+        occurance = Counter(words)
         if occurance[the_word]:
             return 1
         return 0
         
 if __name__ == "__main__":
     l = HasWordFilter()
-    email = 'ahoj jojofnkc  hns s ha ahs money hsl'
+    email = 'ahoj jojofnkc  hns s ha ahs mOney hsl'
     print(l.test(email, 'money'))
 
 
