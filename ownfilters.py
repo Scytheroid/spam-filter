834c7ac90b66ac364101ac9f2949d93ffe724080
houskkam <kamila.hou@seznam.cz>
2020-12-23 17:41:45 +0100

    hasWord update

--- ownfilters.py
+++ ownfilters.py
@@ -3,17 +3,44 @@ from basefilter import BaseFilter
 from collections import Counter
 
 class HasWordFilter(BaseFilter):
-    def test(self, email, the_word):
-        words = email.split()
-        occurance = Counter(words)
-        if occurance[the_word]:
-            return 1
-        return 0
+
+    # parses corpus of the email into words, turns all upper letters
+    # into lower letters
+    def __init__(self, email):
+        words = []
+        for word in email.split():
+            words.append(word.lower())
+        self.words = words
+        self.counter = Counter(self.words)
+        self.total = sum(self.counter.values())
+    
+    # returns how many times there is a spam word
+    def occurs(self, spam_word):
+        return self.counter[spam_word]
+        
+    def spam_words(self):
+        # words that are usually in spams from 
+        # www.inc.com/geoffrey-james/how-to-avoid-a-spam-filter-5-rules.html
+        sp_words = ['advertisement', 'business', 'cash', 'cheap', 'commodity', \
+        'congratulations', 'credit', 'deal', 'debt', 'degree', 'disclaimer', \
+        'discount', 'free', 'gimmick', 'guarantee', 'income', 'ink', \
+        'investment', 'joke', 'load', 'marketing', 'merchant', 'money', \
+        'obligation', 'offer', 'opt', 'opportunity', 'outstanding', 'payoff', \
+        'price', 'profit', 'promo', 'promotion', 'rate', 'refund', 'rich', \
+        'sales', 'save', 'shop', 'spam', 'spree', 'stock', 'subscribe', \
+        'trading', 'wealth', 'win', 'winner', 'winning', 'won']
+        sp_words_total = 0
+        for spam_word in sp_words:
+            sp_words_total += self.counter[spam_word]
+        return sp_words_total
+        
+    def test(self):
+        pass
         
 if __name__ == "__main__":
-    l = HasWordFilter()
-    email = 'ahoj jojofnkc  hns s ha ahs mOney hsl'
-    print(l.test(email, 'money'))
-
-
+    email = 'ahoj jojoPOIJSDnkc  hns MONey RICH RICH s ha ahs mOney hsl'
+    l = HasWordFilter(email)
+    wordiis = []
+    print(l.spam_words())
+    
     
