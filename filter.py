8bf09eeda2b8a005ecf86bd8224a5e4f3261f96b
Scytheroid <bohdan.kopcak@gmail.com>
2020-12-24 09:40:15 +0100

    Not tested, mofified to be a test basin

--- filter.py
+++ filter.py
@@ -1,28 +1,50 @@
 import os
+import inspect
 
 from constants import POSITIVE, NEGATIVE
+from corpus import Corpus
 import ownfilters
+import utils
 
 class MyFilter:
     def __init__(self):
-        self.filter_importance = 2
+        # self.filter_importance = 2
         # initializing importance of our filters in case they don't let us
         # train them
+        self.filters = []
+        self.init_filters()
+
+    def init_filters(self):
+        for name, obj in inspect.getmembers(ownfilters):
+            if inspect.isclass(obj):
+                if obj.__module__ == "ownfilters":
+                    print("Initializing " + name)
+                    filt = obj.__init__(obj)
+                    self.filters.append(filt)
+    
+    def train(self, dir_path):
+        corpus = Corpus(dir_path)
+
+        for filt in self.filters:
+            print("Training " + filt.__name__)
+            filt.train(corpus)
 
     def test(self, dir_path):
-        with open(dir_path + "/!prediction.txt","w+", encoding = "utf-8") as f:
-            for fname in os.listdir(dir_path):
-                if not fname.startswith('!'):
-                    #analyzing the file
-                    result = POSITIVE
-                    f.write(fname + " " + result +"\n") # result
-                    
-    def train(self, dir_path):
-        pass
+        corpus = Corpus(dir_path)
+        clasif = dict()
         
+        for name, mail in corpus.emails():
+            for filt in self.filters:
+                print("Testing " + name + " with " + filt.__name__)
+                result = filt.test(mail)
+                print("Result is: " + result)
+            clasif[name] = POSITIVE
+        
+        utils.write_classification_to_file(clasif, dir_path + "/!prediction.txt")
+                    
+            
 if __name__ == '__main__':
-    c = MyFilter()
+    filt = MyFilter()
     corpus_dir = '2/'
-    c.train(corpus_dir)
-    c.test(corpus_dir)
+    filt.train(corpus_dir)
             
