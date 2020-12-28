f301cf4ae7f9509d74082371b9e7e58b0cc90799
Scytheroid <bohdan.kopcak@gmail.com>
2020-12-28 19:52:10 +0100

    Added blacklist filter

--- ownfilters.py
+++ ownfilters.py
@@ -3,7 +3,28 @@ from basefilter import BaseFilter, WordFilter
 from collections import Counter
 from corpus import Corpus
 from trainingcorpus import TrainingCorpus
+import email.message
 
+class BlacklistFilter(BaseFilter):
+    def __init__(self):
+        BaseFilter.__init__(self)
+        self.blacklist = set()
+
+    def train(self, training_corpus):
+        for fname, mail in training_corpus.spams():
+            from_header = email.message_from_string(mail).get('From').split(' ')
+            self.blacklist.add(from_header[-1])
+    
+    def test(self, mail):
+        from_header = email.message_from_string(mail).get('From').split(' ')
+        suspicious_address = from_header[-1]
+        if suspicious_address in self.blacklist:
+            return 1    # Positive
+        else:
+            return -1    # Can't tell
+
+
+""" ! ! ! WORD FILTERS ! ! ! """
     # analysis of words usually appearing in spams 
     # list of words we used is from: 
     # https://www.inc.com/geoffrey-james/how-to-avoid-a-spam-filter-5-rules.html
