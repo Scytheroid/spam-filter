9aed6d85175683ee45c6b0aa033537e6f5d6d8e1
Scytheroid <bohdan.kopcak@gmail.com>
2020-12-29 12:37:35 +0100

    Basefilter works when uninitialized

--- basefilter.py
+++ basefilter.py
@@ -2,6 +2,8 @@ import os
 from collections import Counter
 
 class BaseFilter:
+    def __init__(self):
+        self.is_trained = False
 
     def train(self, training_corpus):
         pass
@@ -11,16 +13,17 @@ class BaseFilter:
         
     # tests how many times a word has appeared in hams and in spams, then 
     # computes by Bayes the probabilityof mail containing the word being a spam
-    # called from ownfilters.py
+    # called from atomfilters.py
 class WordFilter(BaseFilter):
     def __init__(self, word):
+        BaseFilter.__init__(self)
         self.word_in_spams = 0
         self.word_in_hams = 0
         self.word_total = 0
         self.word = word.lower()
         self.bayes_val = -1
 
-    def train(self, t_corpus):                 
+    def train(self, t_corpus):                
         for name, email in t_corpus.emails():
             email.lower()
             if self.word in email:
@@ -29,7 +32,9 @@ class WordFilter(BaseFilter):
                     self.word_in_spams += 1
                 else:
                     self.word_in_hams += 1
-        self.bayes_val = self.bayes()
+        if self.word_total > 0:
+            self.bayes_val = self.bayes()
+            self.is_trained = True
 
     def test(self, mail):
         mail.lower()
@@ -40,11 +45,7 @@ class WordFilter(BaseFilter):
             return -1
 
     def bayes(self):
-        if self.word_total > 0:
-            return self.word_in_spams / (self.word_in_spams + self.word_in_hams)
-        else:
-            # Don't use me, I have no info
-            return -1
+        return self.word_in_spams / (self.word_in_spams + self.word_in_hams)
 
 if __name__ == "__main__":
     pass
