1cdd6bdeebf2db0a154dafcec81359e43c5f88c7
Scytheroid <bohdan.kopcak@gmail.com>
2020-12-29 12:37:00 +0100

    Added Whitelist filter

--- atomfilters.py
+++ atomfilters.py
@@ -14,7 +14,7 @@ class BlacklistFilter(BaseFilter):
         for fname, mail in training_corpus.spams():
             from_header = email.message_from_string(mail).get('From').split(' ')
             self.blacklist.add(from_header[-1])
-    
+        
     def test(self, mail):
         from_header = email.message_from_string(mail).get('From').split(' ')
         suspicious_address = from_header[-1]
@@ -22,9 +22,28 @@ class BlacklistFilter(BaseFilter):
             return 1    # Positive
         else:
             return -1    # Can't tell
-            
+
+class WhitelistFilter(BaseFilter):
+    def __init__(self):
+        BaseFilter.__init__(self)
+        self.whitelist = set()
+
+    def train(self, training_corpus):
+        for fname, mail in training_corpus.hams():
+            from_header = email.message_from_string(mail).get('From').split(' ')
+            self.whitelist.add(from_header[-1])
+
+    def test(self, mail):
+        from_header = email.message_from_string(mail).get('From').split(' ')
+        suspicious_address = from_header[-1]
+        if suspicious_address in self.whitelist:
+            return 0    # Negative
+        else:
+            return -1    # Can't tell
+
 class HtmlFilter(BaseFilter):
     def __init__(self):
+        BaseFilter.__init__(self)
         self.html_in_ham = 2
         self.html_in_spam = 8
         self.mail_is_spam = 1
@@ -49,6 +68,9 @@ class HtmlFilter(BaseFilter):
                         self.html_in_spam += 1
                     elif t_corpus.is_ham(name):
                         self.html_in_ham += 1
+        
+        if self.mail_is_ham + self.mail_is_ham > 0:
+            self.is_trained = True
 
     def test(self, mail):
         found = 0
