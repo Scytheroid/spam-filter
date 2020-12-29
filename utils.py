3fb52273c3c4ccb22bc071692a5140bec8ce39eb
Scytheroid <bohdan.kopcak@gmail.com>
2020-12-29 12:39:23 +0100

    Added show_mismatched

--- utils.py
+++ utils.py
@@ -22,6 +22,19 @@ def read_only(corpus_dir, which_tag):
             with open(rel_path + name, mode='r', encoding='utf-8') as email:
                     yield name, email.read()   
 
+def show_mismatched(corpus_dir):
+    rel_path = corpus_dir + '/'
+    truth_dict = read_classification_from_file(rel_path + TRUTHFILE)
+    pred_dict = read_classification_from_file(rel_path + '/' + PREDFILE)
+    for name, classif in truth_dict.items():
+        if classif != pred_dict[name]:
+            print("Mismatch found in file {}!".format(name))
+            print("Should be {} but {} is predicted.\n".format(classif, pred_dict[name]))
+            with open(rel_path + name, mode='r', encoding='utf-8') as email:
+                print(email.read())
+            print("\n")
+        a = input()
+
 if __name__ == '__main__':
     corpus_dir = '1'
     read_only(corpus_dir, NEGATIVE)
